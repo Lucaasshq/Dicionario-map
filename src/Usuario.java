3f881e0367a3e7e5a535ef4a731b23265eaf717e
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-10 15:33:04 -0300

    concluido

--- src/Usuario.java
+++ src/Usuario.java
@@ -4,7 +4,7 @@ public class Usuario {
         Dicionario dicionario = new Dicionario();
 
         dicionario.adicionarPalavra("Paz", "Estado de calmaria");
-        dicionario.adicionarPalavra("Pai", "Aquele que tem");
+        dicionario.adicionarPalavra("Paz", "Aquele que tem");
         dicionario.adicionarPalavra("Empatia", "Capacidade de se colocar no lugar de outra pessoa");
         dicionario.adicionarPalavra("Refutar", "Derrubar as afirmações alheias usando alegações");
         dicionario.adicionarPalavra("Peculiar", "Caracteristica de algo ou alguém");
